8a60232afb8a071b12e18e13c22006df8125edce
anduhrooo <yang.andrew7@gmail.com>
2024-03-07 12:00:11 -0800

    updates

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,8 @@ const riddle = document.getElementById('riddle')
 const weatherSearch = document.getElementById('weatherSearch')
 const weatherResults = document.getElementById('weatherResults')
 const news = document.getElementById('query')
-
+const clickMe = document.getElementById('clickMe')
+const modal = document.querySelectorAll('.modal')
 
 date.textContent = 'Today is ' + today.format('MMMM D, YYYY');
 
@@ -329,8 +330,60 @@ cards.forEach(card => {
 //     });
 //   });
 // });
-=======
   
-  }
-  triviaCard();
+triviaCard();
+
+
+//for opening modal
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Functions to open and close a modal
+//   function openModal($el) {
+//     $el.classList.add('is-active');
+//   }
+
+//   function closeModal($el) {
+//     $el.classList.remove('is-active');
+//   }
+
+//   function closeAllModals() {
+//     (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+//       closeModal($modal);
+//     });
+//   }
+
+//   // Add a click event on buttons to open a specific modal
+//   (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+//     const modal = $trigger.dataset.target;
+//     const $target = document.getElementById(modal);
+
+//     $trigger.addEventListener('click', () => {
+//       openModal($target);
+//     });
+//   });
+
+//   // Add a click event on various child elements to close the parent modal
+//   (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+//     const $target = $close.closest('.modal');
+
+//     $close.addEventListener('click', () => {
+//       closeModal($target);
+//     });
+//   });
+
+//   // Add a keyboard event to close all modals
+//   document.addEventListener('keydown', (event) => {
+//     if(event.key === "Escape") {
+//       closeAllModals();
+//     }
+//   });
+// });
+function openModal() {
+  // modal.classList.add('is-active')
+  console.log('click')
+}
+
+clickMe.addEventListener('click', openModal)
+
+
+
 
