c87c15b09561bcfff89f4ef39ac3f2c487acf8fd
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-03-07 10:19:37 -0800

    tried code for draggable cards

--- assets/script.js
+++ assets/script.js
@@ -11,130 +11,106 @@ const date = document.getElementById('date');
 const time = document.getElementById('time');
 const weather = document.getElementById('weather');
 const workspace = document.getElementById('workspace');
-const trivia = document.getElementById('trivia')
+const riddle = document.getElementById('riddle')
 
 date.textContent = 'Today is ' + today.format('MMMM D, YYYY');
 
 function displayTime() {
-    const currentTime = dayjs().format(' hh:mm:ss a');
-    time.textContent = 'The Time is ' + currentTime;
+  const currentTime = dayjs().format(' hh:mm:ss a');
+  time.textContent = 'The Time is ' + currentTime;
 }
 
 setInterval(displayTime, 1000);
 
 // WEATHER API
 function getWeather(city, state, country) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&appid=9ee9271b9afcecf4a8ec627a8439aa7b&units=imperial")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            console.log(data.weather[0].main);
-            console.log(data.main.temp);
-            console.log(data.sys.sunrise);
-            console.log(data.sys.sunset);
-            const createElement = document.createElement('p');
-            createElement.textContent = data.weather[0].main + 'the temperature is ' + data.main.temp + 'sunrise: ' + data.sys.sunrise + 'sunset ' + data.sys.sunset;
-            weather.append(createElement);
-        });
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&appid=9ee9271b9afcecf4a8ec627a8439aa7b&units=imperial")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.weather[0].main);
+      console.log(data.main.temp);
+      console.log(data.sys.sunrise);
+      console.log(data.sys.sunset);
+      const createElement = document.createElement('p');
+      createElement.textContent = data.weather[0].main + 'the temperature is ' + data.main.temp + 'sunrise: ' + data.sys.sunrise + 'sunset ' + data.sys.sunset;
+      weather.append(createElement);
+    });
 }
 
 getWeather("irvine", "california", "usa");
 
 //Ron Swanson quote API
 function ronSwanson() {
-    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            const createElement = document.createElement('p');
-            createElement.textContent = `"` + data + `"`;
-            ronQuote.append(createElement);
-        });
+  fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      const createElement = document.createElement('p');
+      createElement.textContent = `"` + data + `"`;
+      ronQuote.append(createElement);
+    });
 }
 
 ronSwanson();
 
 //Harry Potter Quotes API
 function harryPotter() {
-    fetch("https://api.portkey.uk/quote")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            const createElement = document.createElement('p');
-            createElement.textContent = data.quote + data.speaker + data.story;
-            harryPotterQuote.append(createElement);
-        });
+  fetch("https://api.portkey.uk/quote")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      const createElement = document.createElement('p');
+      createElement.textContent = data.quote + data.speaker + data.story;
+      harryPotterQuote.append(createElement);
+    });
 }
 
 harryPotter();
 
 //Random Facts API
 function randomFactCard() {
-    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data.text);
-            const createElement = document.createElement('p');
-            createElement.textContent = [data.text];
-            randomFact.append(createElement);
-        });
+  fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.text);
+      const createElement = document.createElement('p');
+      createElement.textContent = [data.text];
+      randomFact.append(createElement);
+    });
 }
 
 
 randomFactCard();
 
 
-// trivia question API
-function triviaCard() {
-  const apiKey= 'e//0Bd/EB6LkCS4euqvh5w==kbmISpXkV7swqkZO'
-  fetch('https://api.api-ninjas.com/v1/riddles', {
-    headers: {
-      'X-Api-Key': apiKey,
-    },
-  })
-  .then(function(response){
-    return response.json();
-  }).then (function(data){
-    console.log(data)
-    console.log(data[0].title)
-    console.log(data[0].question)
-    console.log(data[0].answer)
-    const createElement = document.createElement('p')
-    createElement.textContent = 'Title: ' + data[0].title + 'question' + data[0].question + 'answer' + data[0].answer
-    trivia.append(createElement)
-  })
-
-}
-triviaCard();
-
-
 function setBookmark(website) {
-    const createNewSection = document.createElement('section');
-    const createAElement = document.createElement('a');
-    createNewSection.classList.add('card');
-    createAElement.textContent = website;
-    createAElement.href = website;
-    createNewSection.append(createAElement);
-    workspace.append(createNewSection);
+  const createNewSection = document.createElement('section');
+  const createAElement = document.createElement('a');
+  createNewSection.classList.add('card');
+  createAElement.textContent = website;
+  createAElement.href = website;
+  createNewSection.append(createAElement);
+  workspace.append(createNewSection);
 }
 
 // news API
 document.addEventListener("DOMContentLoaded", function () {
   const apiKey = "pub_394864a477ec1ed1904cc024ec9c031746837";
-  
+
   // Function to fetch news data based on user query
   function fetchNews(query) {
     const language = "en";
     const url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${query}&language=${language}`;
-    
+
     fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -145,7 +121,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .then(data => {
         const newsContainer = document.getElementById("news-container");
         newsContainer.innerHTML = ''; // Clear previous results
-        
+
         // Loop through the first three news items
         for (let i = 0; i < 3; i++) {
           const item = data.results[i];
@@ -177,9 +153,9 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Error fetching data:", error);
       });
   }
-  
+
   // Event listener for search button click
-  document.getElementById("search-btn").addEventListener("click", function() {
+  document.getElementById("search-btn").addEventListener("click", function () {
     const query = document.getElementById("query-input").value;
     if (query.trim() !== "") {
       fetchNews(query);
@@ -189,95 +165,143 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
- // SPORTS API
- const myHeaders = new Headers();
- myHeaders.append("x-rapidapi-key", "2eb6e48453a25b9ed67a39562fe656a3");
- myHeaders.append("x-rapidapi-host", "v1.basketball.api-sports.io");
- 
- const requestOptions = {
-   method: 'GET',
-   headers: myHeaders,
-   redirect: 'follow'
- };
+// SPORTS API
+const myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "2eb6e48453a25b9ed67a39562fe656a3");
+myHeaders.append("x-rapidapi-host", "v1.basketball.api-sports.io");
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
 
- //TODO: maybe ask for user input and display prior three scores from favorite team?
- function getSports(date) {
-     fetch("https://v1.basketball.api-sports.io/games?league=12&season=2023-2024&date=" + date, requestOptions)
-         .then(function (response) {
-           return response.json();
-         }).then (function(data){
-           //TODO: need to create a for loop for this lol
-           const createNewSection0 = document.createElement('section')
-           const createAwayTeam0 = document.createElement('h4')
-           const createAwayScore0 = document.createElement('p')
-           const createHomeTeam0 = document.createElement('h4')
-           const createHomeScore0 = document.createElement('p')
-           createNewSection0.classList.add('miniCards')
-           createAwayTeam0.textContent = data.response[0].teams.away.name
-           createAwayScore0.textContent = data.response[0].scores.away.total
-           createNewSection0.append(createAwayTeam0)
-           createNewSection0.append(createAwayScore0)
-           createHomeTeam0.textContent = data.response[0].teams.home.name
-           createHomeScore0.textContent = data.response[0].scores.home.total              
-           createNewSection0.append(createHomeTeam0)
-           createNewSection0.append(createHomeScore0)
-           nbaScores.append(createNewSection0)
-           //seriously...
-           const createNewSection1 = document.createElement('section')
-           const createAwayTeam1 = document.createElement('h4')
-           const createAwayScore1 = document.createElement('p')
-           const createHomeTeam1 = document.createElement('h4')
-           const createHomeScore1 = document.createElement('p')
-           createNewSection1.classList.add('miniCards')
-           createAwayTeam1.textContent = data.response[1].teams.away.name
-           createAwayScore1.textContent = data.response[1].scores.away.total
-           createNewSection1.append(createAwayTeam1)
-           createNewSection1.append(createAwayScore1)
-           createHomeTeam1.textContent = data.response[1].teams.home.name
-           createHomeScore1.textContent = data.response[1].scores.home.total              
-           createNewSection1.append(createHomeTeam1)
-           createNewSection1.append(createHomeScore1)
-           nbaScores.append(createNewSection1)
-           //this is ridiculous...
-           const createNewSection2 = document.createElement('section')
-           const createAwayTeam2 = document.createElement('h4')
-           const createAwayScore2 = document.createElement('p')
-           const createHomeTeam2 = document.createElement('h4')
-           const createHomeScore2 = document.createElement('p')
-           createNewSection2.classList.add('miniCards')
-           createAwayTeam2.textContent = data.response[2].teams.away.name
-           createAwayScore2.textContent = data.response[2].scores.away.total
-           createNewSection2.append(createAwayTeam2)
-           createNewSection2.append(createAwayScore2)
-           createHomeTeam2.textContent = data.response[2].teams.home.name
-           createHomeScore2.textContent = data.response[2].scores.home.total              
-           createNewSection2.append(createHomeTeam2)
-           createNewSection2.append(createHomeScore2)
-           nbaScores.append(createNewSection2)
-         })
-     }
+//TODO: maybe ask for user input and display prior three scores from favorite team?
+function getSports(date) {
+  fetch("https://v1.basketball.api-sports.io/games?league=12&season=2023-2024&date=" + date, requestOptions)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      //TODO: need to create a for loop for this lol
+      const createNewSection0 = document.createElement('section')
+      const createAwayTeam0 = document.createElement('h4')
+      const createAwayScore0 = document.createElement('p')
+      const createHomeTeam0 = document.createElement('h4')
+      const createHomeScore0 = document.createElement('p')
+      createNewSection0.classList.add('miniCards')
+      createAwayTeam0.textContent = data.response[0].teams.away.name
+      createAwayScore0.textContent = data.response[0].scores.away.total
+      createNewSection0.append(createAwayTeam0)
+      createNewSection0.append(createAwayScore0)
+      createHomeTeam0.textContent = data.response[0].teams.home.name
+      createHomeScore0.textContent = data.response[0].scores.home.total
+      createNewSection0.append(createHomeTeam0)
+      createNewSection0.append(createHomeScore0)
+      nbaScores.append(createNewSection0)
+      //seriously...
+      const createNewSection1 = document.createElement('section')
+      const createAwayTeam1 = document.createElement('h4')
+      const createAwayScore1 = document.createElement('p')
+      const createHomeTeam1 = document.createElement('h4')
+      const createHomeScore1 = document.createElement('p')
+      createNewSection1.classList.add('miniCards')
+      createAwayTeam1.textContent = data.response[1].teams.away.name
+      createAwayScore1.textContent = data.response[1].scores.away.total
+      createNewSection1.append(createAwayTeam1)
+      createNewSection1.append(createAwayScore1)
+      createHomeTeam1.textContent = data.response[1].teams.home.name
+      createHomeScore1.textContent = data.response[1].scores.home.total
+      createNewSection1.append(createHomeTeam1)
+      createNewSection1.append(createHomeScore1)
+      nbaScores.append(createNewSection1)
+      //this is ridiculous...
+      const createNewSection2 = document.createElement('section')
+      const createAwayTeam2 = document.createElement('h4')
+      const createAwayScore2 = document.createElement('p')
+      const createHomeTeam2 = document.createElement('h4')
+      const createHomeScore2 = document.createElement('p')
+      createNewSection2.classList.add('miniCards')
+      createAwayTeam2.textContent = data.response[2].teams.away.name
+      createAwayScore2.textContent = data.response[2].scores.away.total
+      createNewSection2.append(createAwayTeam2)
+      createNewSection2.append(createAwayScore2)
+      createHomeTeam2.textContent = data.response[2].teams.home.name
+      createHomeScore2.textContent = data.response[2].scores.home.total
+      createNewSection2.append(createHomeTeam2)
+      createNewSection2.append(createHomeScore2)
+      nbaScores.append(createNewSection2)
+    })
+}
 getSports(yesterday)
 
-// trivia question API
-function triviaCard() {
-    const apiKey= 'e//0Bd/EB6LkCS4euqvh5w==kbmISpXkV7swqkZO'
-    fetch('https://api.api-ninjas.com/v1/riddles', {
-      headers: {
-        'X-Api-Key': apiKey,
-      },
-    })
-    .then(function(response){
+// riddle API
+function riddleCard() {
+  const apiKey = 'e//0Bd/EB6LkCS4euqvh5w==kbmISpXkV7swqkZO'
+  fetch('https://api.api-ninjas.com/v1/riddles', {
+    headers: {
+      'X-Api-Key': apiKey,
+    },
+  })
+    .then(function (response) {
       return response.json();
-    }).then (function(data){
+    }).then(function (data) {
       console.log(data)
       console.log(data[0].title)
       console.log(data[0].question)
       console.log(data[0].answer)
       const createElement = document.createElement('p')
-      createElement.textContent = 'Title: ' + data[0].title + 'question' + data[0].question + 'answer' + data[0].answer
-      trivia.append(createElement)
+      createElement.innerHTML = '<strong>Title: </strong> ' + data[0].title + '<strong><br>Question: </strong>' + data[0].question + '<strong><br>Answer: </strong> ' + data[0].answer;
+      riddle.append(createElement)
     })
-  
-  }
-  triviaCard();
+
+}
+riddleCard();
+
+// draggable cards functions
+const cards = document.querySelectorAll('.card');
+const dropZone = document.getElementById('workspace')
+
+cards.forEach(card => {
+  card.addEventListener('dragstart', () => {
+    card.classList.add('dragging');
+    console.log();
+  });
+  card.addEventListener('dragend', () => card.classList.remove('dragging'));
+  dropZone.addEventListener('drop', () => {
+    dropZone.prepend(card)
+  })
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const draggableCards = document.querySelectorAll('.draggable-card');
+//   const dropColumns = document.querySelectorAll('.dropcolumn');
+
+//   // Add dragstart and dragend listeners to each card
+//   draggableCards.forEach(function (card) {
+//     card.addEventListener('dragstart', function (event) {
+//       event.dataTransfer.setData('text/plain', card.id);
+//       card.classList.add('dragging');
+//     });
+
+//     card.addEventListener('dragend', function (event) {
+//       card.classList.remove('dragging');
+//     });
+//   });
+
+//   // Add dragover and drop listeners to each drop column
+//   dropColumns.forEach(function (dropColumn) {
+//     dropColumn.addEventListener('dragover', function (event) {
+//       event.preventDefault();
+//       event.dataTransfer.dropEffect = 'move';
+//     });
+
+//     dropColumn.addEventListener('drop', function (event) {
+//       event.preventDefault();
+//       const draggedCardId = event.dataTransfer.getData('text/plain');
+//       const draggedCard = document.getElementById(draggedCardId);
+//       dropColumn.appendChild(draggedCard);
+//     });
+//   });
+// });
